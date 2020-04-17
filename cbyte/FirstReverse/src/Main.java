72966009ce9b5d77f8a29e928204642765607da3
Dr Mahike <ignacin@elysium.cc>
2020-04-17 21:22:37 +0200

    Add FirstReverse Challenge

---
+++ cbyte/FirstReverse/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static String FirstReverse(String str) {
+		return new StringBuilder(str).reverse().toString();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(FirstReverse("hello there"));
+	}
+
+}
